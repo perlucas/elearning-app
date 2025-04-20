559d8794fe18a601bc8be697883b96e4d3dd969d
Leonardo Fleita <leo.fleita@gmail.com>
2025-04-20 16:26:09 -0300

    Drag & drop works

--- frontend/src/views/instructors/courses/createCourse/CourseDetails.tsx
+++ frontend/src/views/instructors/courses/createCourse/CourseDetails.tsx
@@ -3,15 +3,49 @@ import { Form, FormControl, FormGroup, FormLabel, Button, Card, Container } from
 import { useTranslation } from 'react-i18next';
 import ModuleItem from './ModuleItem';
 import { Module } from './ModuleItem';
+import {
+    DndContext,
+    DragEndEvent,
+    KeyboardSensor,
+    PointerSensor,
+    TouchSensor,
+    closestCorners,
+    useSensor,
+    useSensors,
+} from '@dnd-kit/core';
+import {
+    arrayMove,
+    SortableContext,
+    sortableKeyboardCoordinates,
+    verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import { useState } from 'react';
 
 const CourseDetails = () => {
     const { t } = useTranslation();
-    const modules: Module[] = [
-        { title: 'REST principles' },
-        { title: 'REST principles' },
-        { title: 'REST principles' },
-    ];
+    const [modules, setModules] = useState<Module[]>([
+        { id: '1', title: 'REST principles 01' },
+        { id: '2', title: 'REST principles 02' },
+        { id: '3', title: 'REST principles 03' },
+    ]);
     //To test the "no modules" state, try using an empty array:
+
+    const handleDragEnd = (event: DragEndEvent) => {
+        const { active, over } = event;
+        if (!over || active.id === over.id) return;
+        const originalIdx = modules.findIndex((m) => m.id === active.id);
+        const newIdx = modules.findIndex((m) => m.id === over.id);
+        setModules(arrayMove(modules, originalIdx, newIdx));
+    };
+
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(TouchSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        }),
+    );
+
     return (
         <section className="p-0">
             <Form>
@@ -28,14 +62,18 @@ const CourseDetails = () => {
                         {t('views.instructors.courses.createCourse.addModule')}
                     </Button>
                 </div>
-                <Card>
-                    <Container className="g-0">
-                        {modules.length > 0 ? (
-                            modules.map((mod, index) => <ModuleItem module={mod} index={index} key={index} />)
-                        ) : (
-                            <p>{t('views.instructors.courses.createCourse.noModules')}</p>
-                        )}
-                    </Container>
+                <Card className="p-0">
+                    <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd} sensors={sensors}>
+                        <Container fluid>
+                            <SortableContext items={modules.map((m) => m.id)} strategy={verticalListSortingStrategy}>
+                                {modules.length > 0 ? (
+                                    modules.map((mod) => <ModuleItem module={mod} modules={modules} key={mod.id} />)
+                                ) : (
+                                    <p>{t('views.instructors.courses.createCourse.noModules')}</p>
+                                )}
+                            </SortableContext>
+                        </Container>
+                    </DndContext>
                 </Card>
             </section>
         </section>
--- frontend/src/views/instructors/courses/createCourse/ModuleItem.tsx
+++ frontend/src/views/instructors/courses/createCourse/ModuleItem.tsx
@@ -1,19 +1,34 @@
 import { Col, Row } from 'react-bootstrap';
 import { BsList, BsPencilSquare, BsPlus, BsTrash } from 'react-icons/bs';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 export interface Module {
+    id: string;
     title: string;
 }
 
 interface ModuleItemProps {
     module: Module;
-    index: number;
+    modules: Module[];
 }
 
-const ModuleItem: React.FC<ModuleItemProps> = ({ module, index }) => {
+const ModuleItem: React.FC<ModuleItemProps> = ({ module, modules }) => {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: module.id });
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+    };
+
+    const index = modules.findIndex((m) => m.id === module.id);
     return (
-        <Row className="bg-secondary d-flex align-items-center g-0 px-2 py-2 text-dark my-2">
-            <Col xs="auto" className="px-1">
+        <Row
+            className="d-flex align-items-center px-2 py-2 text-dark my-2 __module-item"
+            ref={setNodeRef}
+            style={style}
+        >
+            <Col xs="auto" className="px-1 __module-item-draggable" {...attributes} {...listeners}>
                 <BsList />
             </Col>
             <Col className="px-1">
